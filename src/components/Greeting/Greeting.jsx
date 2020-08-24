2cf0d1f0d1b28fa730ecc0d71ed437a3f97559c4
lu <lyubov.kolosova1997@mail.ru>
2020-08-24 10:27:14 +0500

    добавлена тестовая страница приветствие

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import './Greeting.css';
 export class Greeting extends React.Component {
     constructor(props) {
         super(props);
+        this.getData = this.getData.bind(this);
     }
     render() {
         return (
-            <div>Блок с приветствием</div>
+            <div className="greetingPage">
+                <p>При нажатии на кнопку отправляем запрос к серверу, метод там парсит данные с сайта,
+                    собирает их в объект и возвращает сюда.</p>
+                <div className="btnGreeting">Получить данные</div>
+                <div onClick={this.getData} className="contentGreeting"></div>
+            </div>
         ) ;
+    }
+
+    getData(){
+
     }
 }
